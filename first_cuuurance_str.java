a0b250f8aaca8f3a53f2f44aef4033d87eb51731
Parth Sahu <work.parthsahu@gmail.com>
2026-08-30 23:39:26 +0530

    remove given element ...

--- first_cuuurance_str.java
+++ first_cuuurance_str.java
@@ -2,6 +2,9 @@ public class first_cuuurance_str {
 
     public static int strStr(String haystack, String needle) {
 
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
         for (int i = 0; i <= haystack.length() - needle.length(); i++) {
 
             boolean match = true;
